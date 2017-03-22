80f909c65264fd9c89bd5628a924f30419fa07c8
artytim <arty.tim@gmail.com>
2017-03-22 22:48:16 +0800

    Data Management Layer - Adding CRUD methods to the content provider

--- app/src/main/java/com/artytect/android/artysdumbnotes/DBOpenHelper.java
+++ app/src/main/java/com/artytect/android/artysdumbnotes/DBOpenHelper.java
@@ -20,8 +20,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     public static final String NOTE_TEXT = "noteText";
     public static final String NOTE_CREATED = "noteCreated"; // Date & time, used in sorting
 
-    public static final String[] ALL_COLUMNS =
-            {NOTE_ID, NOTE_TEXT, NOTE_CREATED};
+    public static final String[] ALL_COLUMNS = {NOTE_ID, NOTE_TEXT, NOTE_CREATED};
 
     //SQL to create table
     private static final String TABLE_CREATE =
--- app/src/main/java/com/artytect/android/artysdumbnotes/MainActivity.java
+++ app/src/main/java/com/artytect/android/artysdumbnotes/MainActivity.java
@@ -1,10 +1,13 @@
 package com.artytect.android.artysdumbnotes;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        insertNote("New note");
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
     @Override
