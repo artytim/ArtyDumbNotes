300f200038accb0603201ff568440a0e1a373d35
artytim <arty.tim@gmail.com>
2017-03-22 23:24:11 +0800

    Data Management Layer - Displaying data with a ListView

--- app/src/main/java/com/artytect/android/artysdumbnotes/MainActivity.java
+++ app/src/main/java/com/artytect/android/artysdumbnotes/MainActivity.java
@@ -1,16 +1,20 @@
 package com.artytect.android.artysdumbnotes;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         insertNote("New note");
+
+        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI,
+                DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+        String[] from = {DBOpenHelper.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
     }
 
     private void insertNote(String noteText) {
